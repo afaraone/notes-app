da03b5b9b7f21b80f5e07b053be73aa3eeef7724
Jon Urry <jon@urry.me>
2018-10-16 17:50:37 +0100

    refactored connect to mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,22 +1,38 @@
 const mongoURL = 'mongodb://localhost:27017/';
 const MongoClient = require('mongodb').MongoClient;
 
-module.exports.getNotes = res => {
+const connect = (
+  res,
+  callback,
+  dbName = 'test-notes',
+  dbCollection = 'notes'
+) => {
   MongoClient.connect(
     mongoURL,
     { useNewUrlParser: true }
   )
     .then(client => {
-      const db = client.db('test-notes');
-      const collection = db.collection('notes');
-      collection
-        .find({})
-        .toArray()
-        .then(response => {
-          //console.log(response);
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(response));
-        });
+      const db = client.db(dbName);
+      const collection = db.collection(dbCollection);
+      callback(res, collection);
     })
     .catch(error => console.error(error));
+};
+
+const getNotesFromCollection = (res, collection) => {
+  collection
+    .find({})
+    .toArray()
+    .then(response => {
+      //console.log(response);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(response));
+    });
+};
+
+module.exports.getNotes = res => {
+  connect(
+    res,
+    getNotesFromCollection
+  );
 };
