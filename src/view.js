5230ae19562f79309f53e094f9d3438a83477561
Andres Faraone <andresfaraone@outlook.com>
2018-10-18 18:19:10 +0100

    fixed view

--- src/view.js
+++ src/view.js
@@ -8,17 +8,29 @@ class View {
   async renderNotes(notes) {
     let noteElement = this.builder.getElementById('view-note-span');
     for (let note of notes) {
+      // set names of elements
       let noteId = note._id.toString()
-      console.log(noteId)
       let noteDivId = 'noteDiv' + noteId
+      let noteDeleteId = 'delete' + noteId
+      let noteEditId = 'edit' + noteId
+
+      // create div
       let noteDiv = this.builder.createNode('div', noteDivId)
       this.builder.addNode(noteDiv, 'view-note-span');
+
+      // create text, edit and delete buttons
       let noteText = this.builder.createNode('p', noteId);
-      let noteDeleteId = 'delete' + noteId
+      let noteEdit = this.builder.createNode('button', noteEditId)
       let noteDelete = this.builder.createNode('button', noteDeleteId)
+
+      // add elements to div
       this.builder.addNode(noteText, noteDivId)
+      this.builder.addNode(noteEdit, noteDivId)
       this.builder.addNode(noteDelete, noteDivId)
+
+      // add text to elements
       this.builder.updateText(noteId, note.title);
+      this.builder.updateText(noteEditId, "Edit");
       this.builder.updateText(noteDeleteId, "Delete");
     }
   }
